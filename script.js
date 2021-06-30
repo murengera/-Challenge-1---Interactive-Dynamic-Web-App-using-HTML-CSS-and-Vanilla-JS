105ca3fb060754a4e9d57c5a1579286fe6729df1
Your Name <you@example.com>
2021-06-30 10:47:35 +0200

    update js function

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-let html="";
-const userList=document.getElementById('user');
-const userDetailsContent=document.querySelector('.user-details-content');
-const userCloseBtn=document.getElementById('user-close-btn');
-// event listners
-//user details
-userList.addEventListener('click',getUserDetails);
-userCloseBtn.addEventListener('click',()=>{
-    userDetailsContent.parentElement.classList.remove('showUser');
+// variable declaration
+let html = "";
+const userList = document.getElementById('user');
+const userDetailsContent = document.querySelector('.user-details-content');
+const userCloseBtn = document.getElementById('user-close-btn');
+
+
+
+
+//variable assignements
+
+userList.addEventListener('click', getUserPost);
+userCloseBtn.addEventListener('click', () => {
+  userDetailsContent.parentElement.classList.remove('showUser');
 })
 
-// get user list from api
-function getUserList(){
-  
-   fetch(`https://jsonplaceholder.typicode.com/users`).then(response=>response.json())
-   .then(data=>{
-   
-     if(data){
-         data.forEach(user=> {
 
-             html+=`
+
+
+// function of getting user information(name,email only)
+function getUserList() {
+
+  fetch(`https://jsonplaceholder.typicode.com/users`).then(response => {
+
+    if (!response.ok) {
+      throw Error('ERROR');
+    }
+    return response.json();
+  })
+    .then(data => {
+
+      if (data) {
+        data.forEach(user => {
+
+          html += `
              <div class = "user-item" data-id="${user.id}">
              
              <div class = "user-name">
@@ -27,105 +41,68 @@ function getUserList(){
                <a href = "#" class = "user-btn">User post</a>
              </div>
            </div> `
-           ;
-         });
-     }
-     userList.innerHTML=html
-   })
+            ;
+        });
+      }
+      userList.innerHTML = html
+    }).catch(error => {
+      alert("Please check your internet connection")
+    }
+
+
+    )
 }
+
+
 
 
 // function for getting posts of every user
 
-function getUserDetails(e){
-    e.preventDefault();
-    if(e.target.classList.contains('user-btn')){
-        let userItem=e.target.parentElement.parentElement;
-        fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userItem.dataset.id}`)
-        .then(response=>{
-            if(!response){
-                throw Error("ERROR")
-            }
-            return response.json();
-        }).then(data=>{
-            console.log(data)
-        const html=data
-        .map(user=>{
-          
-               return `
+function getUserPost(id) {
+  id.preventDefault();
+  if (id.target.classList.contains('user-btn')) {
+    let userPost = id.target.parentElement.parentElement;
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userPost.dataset.id}`)
+      .then(response => {
+
+
+        if (!response) {
+          throw Error("ERROR")
+        }
+
+
+        return response.json();
+      }).then(data => {
+
+
+        const html = data
+
+          .map(user => {
+
+            return `
 
                <div class = "user-post">
-               
-               <p  id="title1"style="color:black"> title</p>
-               
                <p  id="title"> ${user.title} </p>
 
-               <p id="body1"> body</p>
                <span id="body">   ${user.body}</span>
            
-               </div>
+               </div> 
+
               
-              
+              `;
+          }).join("");
 
-
-
-
-
-
-          
-          
-          
-        
-               
-            
-               
-               
-               
-               
-
-                 
-  
-  
-  
-  
-  
-  
-  
-  
-
-  
-
-    
-    
-
-  
-
-               
-               </div>`;
-        }).join("");
-          userDetailsContent.innerHTML=html;
+        userDetailsContent.innerHTML = html;
         userDetailsContent.parentElement.classList.add('showUser')
 
+      }).
+      catch(error => {
+        alert("please check your connection")
+      });
 
-
-
-        
-      
-        }).
-        catch(error=>{console.log(error)});
-
-
-    }
-
+  }
 
 }
 
-
-
-
-
-
-
-
-
+// callling function of getting information of user including user name and email only
 getUserList();
